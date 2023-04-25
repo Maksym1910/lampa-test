c5e6ec19fb17be10fc2e7eb5d8e93a2564b2cced
Maksym Rozhytskyi <maksym.rozhytskyi@gmail.com>
2023-04-25 18:24:44 +0300

    folder structure modifications

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import './index.scss';
-import App from './App';
+import 'app/index.scss';
+import App from 'app/App';
 
 const app = document.getElementById('app');
 const root = createRoot(app);
