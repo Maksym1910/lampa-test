692a85ba04d084a05d980cc6d66e33a8e90473a0
Maksym Rozhytskyi <maksym.rozhytskyi@gmail.com>
2023-04-25 12:49:33 +0300

    eslint config modifications

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,12 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    'webpack',
+    'webpack.config.js',
+  ],
   plugins: [
     'react',
     'jsx-a11y',
