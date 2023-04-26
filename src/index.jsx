5350b5a897d62da5877bb22018a6556d863a2c08
Maksym Rozhytskyi <maksym.rozhytskyi@gmail.com>
2023-04-26 20:49:46 +0300

    refactor: remove global styles import

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import 'app/index.scss';
 import App from 'app/App';
 
 const app = document.getElementById('app');
