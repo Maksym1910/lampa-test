0647573066f249cddd46258a11f74c77f7cd11f1
Maksym Rozhytskyi <maksym.rozhytskyi@gmail.com>
2023-04-26 23:50:38 +0300

    feat: add products component

---
+++ src/features/Products/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Products = () => (
+  <div>
+    <h1>Products</h1>
+    <Link to="/cart">Cart</Link>
+  </div>
+);
+
+export default Products;
