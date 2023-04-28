ce8b9e5a649f2900ec5c72dfb7f3caf6b9d686bb
Maksym Rozhytskyi <maksym.rozhytskyi@gmail.com>
2023-04-28 20:01:48 +0300

    feat: public Cart API

--- src/features/Cart/index.tsx
+++ src/features/Cart/index.tsx
@@ -1,9 +1,4 @@
-import React from 'react';
-
-const Cart = () => (
-  <div>
-    Cart
-  </div>
-);
-
-export default Cart;
+export { AddToCart } from './components/AddToCart/AddToCart';
+export { CartNavigationButton } from './components/CartNavigationButton/CartNavigationButton';
+export { TotalCartPrice } from './components/TotalCartPrice/TotalCartPrice';
+export { addProductsToCart } from './store/cartSlice';
