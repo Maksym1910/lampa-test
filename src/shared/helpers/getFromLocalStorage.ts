96508f0452153a313c5eca85c7e70c49a4c22929
Maksym Rozhytskyi <maksym.rozhytskyi@gmail.com>
2023-04-30 12:32:32 +0300

    refactor: remove getFromLocalStorage helper

--- src/shared/helpers/getFromLocalStorage.ts
+++
@@ -1,4 +0,0 @@
-export const getFromLocalStorage = (name: string, defaultValue: string) => {
-  const value = localStorage.getItem(name);
-  return value ? JSON.parse(value) : JSON.parse(defaultValue);
-};
