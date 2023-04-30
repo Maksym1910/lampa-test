035a2e9ce278427db3c6f9675240a0ceca5d9c38
Maksym Rozhytskyi <maksym.rozhytskyi@gmail.com>
2023-04-30 20:06:21 +0300

    refactor: use Loader component

--- src/app/providers/withSuspense.tsx
+++ src/app/providers/withSuspense.tsx
@@ -1,7 +1,8 @@
 import React, { Suspense } from 'react';
+import { Loader } from 'shared/components/Loader';
 
 export const withSuspense = (WrappedComponent: React.FunctionComponent) => () => (
-  <Suspense fallback={<div>Loading...</div>}>
+  <Suspense fallback={<Loader />}>
     <WrappedComponent />
   </Suspense>
 );
