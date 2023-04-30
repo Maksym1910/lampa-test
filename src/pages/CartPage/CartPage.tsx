482346508fe70718d0ad27ca7902e6542c9c8496
Maksym Rozhytskyi <maksym.rozhytskyi@gmail.com>
2023-04-30 12:25:06 +0300

    refactor: change files names and structure CartPage

---
+++ src/pages/CartPage/CartPage.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Header } from 'widgets/Header';
+import { CartProductsList } from 'widgets/CartProductsList';
+
+// import { OrderForm } from 'features/OrderForm';
+
+import styles from './CartPage.module.scss';
+
+export const CartPage = () => (
+  <>
+    <Header />
+    <div className={styles.root}>
+      <CartProductsList />
+      {/* <OrderForm /> */}
+    </div>
+  </>
+
+);
--- src/pages/CartPage/index.tsx
+++ src/pages/CartPage/index.tsx
@@ -1,14 +1,1 @@
-import React from 'react';
-
-import { Header } from 'widgets/Header';
-
-import { CartProductsList } from 'widgets/CartProductsList';
-
-const CartPage = () => (
-  <>
-    <Header />
-    <CartProductsList />
-  </>
-);
-
-export default CartPage;
+export { CartPage } from './CartPage';
