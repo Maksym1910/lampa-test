205631f3fc966d1affe2d84b538bb91d1221d569
Maksym Rozhytskyi <maksym.rozhytskyi@gmail.com>
2023-04-30 12:39:14 +0300

    feat: expose new API endpoint and IProduct type

--- src/entities/Product/api/productApi.ts
+++ src/entities/Product/api/productApi.ts
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
 import { IProductsResponse } from '../model/types';
+import { transformResponse } from '../helpers/transformResponse';
 
 export const productApi = createApi({
   reducerPath: 'productAPI',
@@ -7,17 +8,29 @@ export const productApi = createApi({
     baseUrl: 'https://dummyjson.com',
   }),
   endpoints: (build) => ({
-    fetchAllProducts: build.query<IProductsResponse, number>({
-      query: (limit = 6) => ({
+    fetchAllProducts: build.query<IProductsResponse, number | void>({
+      query: (limit = 20) => ({
         url: '/products',
         params: {
           limit,
+          select: 'id,title,description,price,category',
         },
       }),
+      transformResponse,
+    }),
+    fetchProductsByCategory: build.query<IProductsResponse, string | undefined>({
+      query: (category = undefined) => ({
+        url: `/products/category/${category}`,
+        params: {
+          select: 'id,title,description,price,category',
+        },
+      }),
+      transformResponse,
     }),
   }),
 });
 
 export const {
   useFetchAllProductsQuery,
+  useFetchProductsByCategoryQuery,
 } = productApi;
--- src/entities/Product/index.ts
+++ src/entities/Product/index.ts
@@ -1,3 +1,5 @@
 export { ProductCard } from './components/ProductCard/ProductCard';
-export { priceFormatter } from './helpers/priceFormatter';
+export { formatPrice } from './helpers/formatPrice';
 export { useFetchAllProductsQuery } from './api/productApi';
+export { useFetchProductsByCategoryQuery } from './api/productApi';
+export type { IProduct } from './model/types';
