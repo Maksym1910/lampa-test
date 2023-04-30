89ac58cf6323d157360d400c961fd88545a88ba2
Maksym Rozhytskyi <maksym.rozhytskyi@gmail.com>
2023-04-30 20:04:30 +0300

    refactor: Header component refactor

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { CartNavigationButton, TotalCartPrice } from 'features/Cart';
 
+import { Logo } from 'shared/components/Logo';
 import styles from './Header.module.scss';
 
-export const Header = () => (
-  <header className={styles.root}>
-    <div>Logo</div>
-    <div className={styles.cart}>
-      <TotalCartPrice />
-      <CartNavigationButton />
-    </div>
-  </header>
-);
+export const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLogoClicked = () => {
+    navigate('/products');
+  };
+
+  return (
+    <header className={styles.root}>
+      <Logo onClick={handleLogoClicked} />
+      <section className={styles.cart}>
+        <TotalCartPrice />
+        <CartNavigationButton />
+      </section>
+    </header>
+  );
+};
